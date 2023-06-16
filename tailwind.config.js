7628bc6dc34fec81fca0e150851f6ccdf098c609
ADITYAYOGA06 <adityapratama1890@students.amikom.ac.id>
2023-06-17 06:58:21 +0700

    FE (Nambah project planning, solve dibantu faqih)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js,css}", "./views/*.ejs", "./views/layout/*.ejs"],
+  content: ["./src/*.{html,js,css}", "./views/*.ejs", "./views/layout/*.ejs", "./views/component/*.ejs"],
   theme: {
     extend: {
       // screens: {
